c2e6055878fec588ae32c56a58a6fbdf495ddb52
Obadowski <vobadowski@gmail.com>
2025-05-13 11:24:55 -0300

    feat: Create display and CE buttons
     - Build the DOM (on JS)
     - Create the display + Clear Button

--- scripts/html-design.js
+++ scripts/html-design.js
@@ -1,3 +1,37 @@
 // This file will build the calculator
 
+const MAXLINES = 5;
+
 const mainContainer = document.querySelector(".main-container");
+
+
+function buildRow(mainContainer) {
+
+    const calculatorButton = document.createElement("button");
+    
+    for (let i = 0; i < MAXLINES; i++) {
+        const numbersContainer = document.createElement("div");
+        numbersContainer.classList.add("display");
+        mainContainer.append(numbersContainer);
+        buildButtons(numbersContainer, i);
+    }
+}
+
+function buildButtons(numbersContainer, lineNumber) {
+    if (lineNumber === 0) {
+        appendElement(numbersContainer, "CE", "clear-button");
+        appendElement(numbersContainer, "Calculadora", "calc-screen");
+    } else {
+        
+    }
+
+}
+
+function appendElement(container, textContext, classToAdd) {
+    const element = document.createElement("div");
+    element.classList.add(classToAdd);
+    element.textContent = textContext;
+    container.append(element);
+}
+
+buildRow(mainContainer);
